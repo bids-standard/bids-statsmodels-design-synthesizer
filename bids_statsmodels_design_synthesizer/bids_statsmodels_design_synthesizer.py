a08615c80b5aa4e3ccaffcdd42b6716850fd56dd
leej3 <johnleenimh@gmail.com>
2021-05-06 12:57:30 +0100

    add minimal test of cli

--- bids_statsmodels_design_synthesizer/bids_statsmodels_design_synthesizer.py
+++ bids_statsmodels_design_synthesizer/bids_statsmodels_design_synthesizer.py
@@ -9,8 +9,7 @@ def main():
     args = parser.parse_args()
 
     print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "bids_statsmodels_design_synthesizer.cli.main")
+
     return 0
 
 
--- tests/test_bids_statsmodels_design_synthesizer.py
+++ tests/test_bids_statsmodels_design_synthesizer.py
@@ -3,22 +3,12 @@
 """Tests for `bids_statsmodels_design_synthesizer` package."""
 
 import pytest
-
+import subprocess as sp
 
 from bids_statsmodels_design_synthesizer import bids_statsmodels_design_synthesizer
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_cli_help():
+    output = sp.check_output(["bids_statsmodels_design_synthesizer.py","-h"])
+    with pytest.raises(sp.CalledProcessError):
+        output = sp.check_output(["bids_statsmodels_design_synthesizer.py","--non-existent"])
