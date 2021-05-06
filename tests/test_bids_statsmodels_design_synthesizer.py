78842bd0a2f8ddc3b67d3fa805cfc350de946f31
leej3 <johnleenimh@gmail.com>
2021-05-06 13:16:38 +0100

    add structure of preliminary cli test
    we can parametrize this with pytest for a few test cases.  I was going to
    use some fitlins model examples but start with a minimal example of my
    own construction first, then actually write the implementation and
    iterate over models of increasing complexity/difficulty

--- tests/test_bids_statsmodels_design_synthesizer.py
+++ tests/test_bids_statsmodels_design_synthesizer.py
@@ -12,3 +12,17 @@ def test_cli_help():
     output = sp.check_output(["bids_statsmodels_design_synthesizer.py","-h"])
     with pytest.raises(sp.CalledProcessError):
         output = sp.check_output(["bids_statsmodels_design_synthesizer.py","--non-existent"])
+
+
+def test_minimal_cli_functionality():
+    model = "model-001_smdl.json"
+    bids_dir = "data/..."
+    events_file = "events.tsv"
+    output = sp.check_output([
+        f"""
+        bids_statsmodels_design_synthesizer.py
+        --bids-dir {bids_dir}
+        --model {model}
+        --events-file {events_file}
+        """
+    ])
