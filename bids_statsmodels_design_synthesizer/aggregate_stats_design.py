32ca075309e3e1c88cace7535adf63efc47bf0cf
leej3 <johnleenimh@gmail.com>
2021-05-10 12:57:54 +0100

    add rough prototype of approach

--- bids_statsmodels_design_synthesizer/aggregate_stats_design.py
+++ bids_statsmodels_design_synthesizer/aggregate_stats_design.py
@@ -7,14 +7,18 @@ import json
 from pathlib import Path
 
 from bids_statsmodels_design_synthesizer import transformations
+from bids.utils import convert_JSON
 
 # The following is a hack to avoid writing cli for now
 descriptor_fname = "bids-app-bids-statsmodels-design-synthesizer.json"
-IO_DESCRIPTOR_JSON = Path(__file__).absolute().parent.parent / "boutiques" / descriptor_fname
+IO_DESCRIPTOR_JSON = (
+    Path(__file__).absolute().parent.parent / "boutiques" / descriptor_fname
+)
 if not IO_DESCRIPTOR_JSON.exists():
     IO_DESCRIPTOR_JSON = Path(__file__).parent / descriptor_fname
 if not IO_DESCRIPTOR_JSON.exists():
     raise EnvironmentError("Cannot find the boutiques descriptor to construct the CLI")
+
 
 def main(user_args=None):
     """Console script for bids_statsmodels_design_synthesizer."""
@@ -23,8 +27,24 @@ def main(user_args=None):
         p = PrettyPrinter(desc)
         user_args = p.parser.parse_args()
         user_args = vars(user_args)
-    print(user_args['OUTPUT_DIR'])
+
+    # get specified transformations
+    model_file = Path(user_args["MODEL"])
+    if not model_file.exists():
+        model_file = Path(user_args["BIDS_DIR"]) / "models" / user_args["MODEL"]
+    model = convert_JSON(json.loads(model_file.read_text()))
+    model_transforms = model["steps"][0]["transformations"]
+
+    # Get relevant collection
+    coll = NotImplemented
+
+    # return early for now
     return 0
+    # perform transformations
+    colls = transformations.Transformermanger().transform(coll, model_transforms)
+
+    # Save colls
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
